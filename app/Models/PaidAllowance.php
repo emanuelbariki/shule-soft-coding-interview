41bd0c2811ee01dcde03c345aabbad17af1a5fa6
Emanuel Bariki <emanuelbariki01@gmail.com>
2025-01-03 10:40:41 +0300

    Logic: added Relationships to the rest of the models
    Logic: added Relationships to the rest of the models

--- app/Models/PaidAllowance.php
+++ app/Models/PaidAllowance.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PaidAllowance extends Model
 {
     //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function allowance()
+    {
+        return $this->belongsTo(Allowance::class);
+    }
 }
