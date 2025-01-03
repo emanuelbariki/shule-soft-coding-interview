7346c3eaecf226127297076fa285be909e511d22
Emanuel Bariki <emanuelbariki01@gmail.com>
2025-01-03 10:37:35 +0300

    Logic: Added Relationships to Employee Model
    Logic: Added Relationships to Employee Model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function gender()
+    {
+        return $this->belongsTo(Gender::class);
+    }
+
+    public function salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
+
+    public function paidSalaries()
+    {
+        return $this->hasMany(PaidSalary::class);
+    }
+
+    public function paidAllowances()
+    {
+        return $this->hasMany(PaidAllowance::class);
+    }
 }
